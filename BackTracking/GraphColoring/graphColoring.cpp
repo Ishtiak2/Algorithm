506bfcfc734508f935044094bfb18ceb7d2b382b
Ishtiak Rahman <ishtiakrahman13579@gmail.com>
2025-09-06 18:58:09 +0600

    update

--- BackTracking/GraphColoring/graphColoring.cpp
+++ BackTracking/GraphColoring/graphColoring.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 const int V = 4; // Number of vertices
+int colors[V] = {0}; // Store colors assigned to vertices
 
 // Function to check if the current color assignment is safe for vertex
 bool isSafe(int vertex, int graph[V][V], int colors[V], int color) {
@@ -13,23 +14,33 @@ bool isSafe(int vertex, int graph[V][V], int colors[V], int color) {
     return true;
 }
 
-// Backtracking function to solve the graph coloring problem
-bool graphColoring(int graph[V][V], int m, int colors[V], int vertex) {
-    if (vertex == V) { // All vertices are assigned a color
-        // Print the solution
-        cout << "Solution: ";
-        for (int i = 0; i < V; i++)
-            cout << colors[i] << " ";
-        cout << endl;
+void printSolution() {
+    // Print the solution
+    cout << "Solution: ";
+    
+    for (int i = 0; i < V; i++)
+        cout << colors[i] << " ";
+
+    cout << endl;
+}
+
+
+bool graphColoring(int graph[V][V], int m, int vertex) {
+    
+    if (vertex == V) {
+        printSolution();
         return true; // Return true to find **one solution**
     }
 
     // Try all colors for the current vertex
+    
     for (int c = 1; c <= m; c++) {
+        
         if (isSafe(vertex, graph, colors, c)) {
+            
             colors[vertex] = c; // Assign color
 
-            if (graphColoring(graph, m, colors, vertex + 1))
+            if (graphColoring(graph, m, vertex + 1))
                 return true; // If solution found, return
 
             colors[vertex] = 0; // Backtrack
@@ -40,7 +51,9 @@ bool graphColoring(int graph[V][V], int m, int colors[V], int vertex) {
 }
 
 int main() {
+    
     // Example graph represented as adjacency matrix
+    
     int graph[V][V] = {
         {0, 1, 1, 0},
         {1, 0, 1, 1},
@@ -49,9 +62,8 @@ int main() {
     };
 
     int m = 3; // Number of colors
-    int colors[V] = {0}; // Store colors assigned to vertices
 
-    if (!graphColoring(graph, m, colors, 0))
+    if (!graphColoring(graph, m, 0))
         cout << "No solution exists with " << m << " colors." << endl;
 
     return 0;
